e01d962b6eb833935492e1412fcbe00d35014e37
Nedko Chulev <ned.doodle@gmail.com>
2018-09-02 15:33:12 +0200

    A lot of stuff lol Idk

--- main.js
+++ main.js
@@ -10,25 +10,28 @@ var comboCounter = 0;
 var comboActiveColor = "#EC6F28";
 var comboInactiveColor = "#eee";
 
-var comboInterval;
 var comboIntervalSpeed = 50;
+var comboInterval = setInterval(drawComboCooldown, comboIntervalSpeed);
 var loadingBarWidth = 1;
 
 var clicks = 0;
 var gameSpeed = 1000;
 var gameLoop = setInterval(loop, gameSpeed);
+var date;
+var timestamp;
+var numbersNotation = true; //True = Scientific; False = Alphabetical
 
 var upgrades = [
-	{HTMLamount: document.getElementById("amount-upgrade0"), HTMLcps: document.getElementById("cps-upgrade0"), HTMLcost: document.getElementById("cost-upgrade0"), HTMLid: document.getElementById("upgrade0"), name: "Upgrade 1", amount: 0, multiplier: 1, cps: 1, cost: 10},
-	{HTMLamount: document.getElementById("amount-upgrade1"), HTMLcps: document.getElementById("cps-upgrade1"), HTMLcost: document.getElementById("cost-upgrade1"), HTMLid: document.getElementById("upgrade1"), name: "Upgrade 2", amount: 0, multiplier: 2, cps: 1, cost: 100},
-	{HTMLamount: document.getElementById("amount-upgrade2"), HTMLcps: document.getElementById("cps-upgrade2"), HTMLcost: document.getElementById("cost-upgrade2"), HTMLid: document.getElementById("upgrade2"), name: "Upgrade 3", amount: 0, multiplier: 3, cps: 1, cost: 1000},
-	{HTMLamount: document.getElementById("amount-upgrade3"), HTMLcps: document.getElementById("cps-upgrade3"), HTMLcost: document.getElementById("cost-upgrade3"), HTMLid: document.getElementById("upgrade3"), name: "Upgrade 4", amount: 0, multiplier: 4, cps: 1, cost: 10000},
-	{HTMLamount: document.getElementById("amount-upgrade4"), HTMLcps: document.getElementById("cps-upgrade4"), HTMLcost: document.getElementById("cost-upgrade4"), HTMLid: document.getElementById("upgrade4"), name: "Upgrade 5", amount: 0, multiplier: 5, cps: 1, cost: 100000},
-	{HTMLamount: document.getElementById("amount-upgrade5"), HTMLcps: document.getElementById("cps-upgrade5"), HTMLcost: document.getElementById("cost-upgrade5"), HTMLid: document.getElementById("upgrade5"), name: "Upgrade 6", amount: 0, multiplier: 6, cps: 1, cost: 1000000},
-	{HTMLamount: document.getElementById("amount-upgrade6"), HTMLcps: document.getElementById("cps-upgrade6"), HTMLcost: document.getElementById("cost-upgrade6"), HTMLid: document.getElementById("upgrade6"), name: "Upgrade 7", amount: 0, multiplier: 7, cps: 1, cost: 10000000},
-	{HTMLamount: document.getElementById("amount-upgrade7"), HTMLcps: document.getElementById("cps-upgrade7"), HTMLcost: document.getElementById("cost-upgrade7"), HTMLid: document.getElementById("upgrade7"), name: "Upgrade 8", amount: 0, multiplier: 8, cps: 1, cost: 100000000},
-	{HTMLamount: document.getElementById("amount-upgrade8"), HTMLcps: document.getElementById("cps-upgrade8"), HTMLcost: document.getElementById("cost-upgrade8"), HTMLid: document.getElementById("upgrade8"), name: "Upgrade 9", amount: 0, multiplier: 9, cps: 1, cost: 1000000000},
-	{HTMLamount: document.getElementById("amount-upgrade9"), HTMLcps: document.getElementById("cps-upgrade9"), HTMLcost: document.getElementById("cost-upgrade9"), HTMLid: document.getElementById("upgrade9"), name: "Upgrade 10", amount: 0, multiplier: 10, cps: 1, cost: 10000000000}
+	{name: "Upgrade 1", HTMLamount: document.getElementById("amount-upgrade0"), HTMLcps: document.getElementById("cps-upgrade0"), HTMLcost: document.getElementById("cost-upgrade0"), HTMLid: document.getElementById("upgrade0"), buttonx10: document.getElementById("upgrade0x10"), buttonx100: document.getElementById("upgrade0x100"), buttonxMAX: document.getElementById("upgrade0xMAX"), amount: 0, multiplier: 1.07, cps: 1, cost: 50},
+	{name: "Upgrade 2", HTMLamount: document.getElementById("amount-upgrade1"), HTMLcps: document.getElementById("cps-upgrade1"), HTMLcost: document.getElementById("cost-upgrade1"), HTMLid: document.getElementById("upgrade1"), buttonx10: document.getElementById("upgrade1x10"), buttonx100: document.getElementById("upgrade1x100"), buttonxMAX: document.getElementById("upgrade1xMAX"), amount: 0, multiplier: 1.07, cps: 1, cost: 250},
+	{name: "Upgrade 3", HTMLamount: document.getElementById("amount-upgrade2"), HTMLcps: document.getElementById("cps-upgrade2"), HTMLcost: document.getElementById("cost-upgrade2"), HTMLid: document.getElementById("upgrade2"), buttonx10: document.getElementById("upgrade2x10"), buttonx100: document.getElementById("upgrade2x100"), buttonxMAX: document.getElementById("upgrade2xMAX"), amount: 0, multiplier: 1.07, cps: 1, cost: 1000},
+	{name: "Upgrade 4", HTMLamount: document.getElementById("amount-upgrade3"), HTMLcps: document.getElementById("cps-upgrade3"), HTMLcost: document.getElementById("cost-upgrade3"), HTMLid: document.getElementById("upgrade3"), buttonx10: document.getElementById("upgrade3x10"), buttonx100: document.getElementById("upgrade3x100"), buttonxMAX: document.getElementById("upgrade3xMAX"), amount: 0, multiplier: 1.07, cps: 1, cost: 4000},
+	{name: "Upgrade 5", HTMLamount: document.getElementById("amount-upgrade4"), HTMLcps: document.getElementById("cps-upgrade4"), HTMLcost: document.getElementById("cost-upgrade4"), HTMLid: document.getElementById("upgrade4"), buttonx10: document.getElementById("upgrade4x10"), buttonx100: document.getElementById("upgrade4x100"), buttonxMAX: document.getElementById("upgrade4xMAX"), amount: 0, multiplier: 1.07, cps: 1, cost: 20000},
+	{name: "Upgrade 6", HTMLamount: document.getElementById("amount-upgrade5"), HTMLcps: document.getElementById("cps-upgrade5"), HTMLcost: document.getElementById("cost-upgrade5"), HTMLid: document.getElementById("upgrade5"), buttonx10: document.getElementById("upgrade5x10"), buttonx100: document.getElementById("upgrade5x100"), buttonxMAX: document.getElementById("upgrade5xMAX"), amount: 0, multiplier: 1.07, cps: 1, cost: 100000},
+	{name: "Upgrade 7", HTMLamount: document.getElementById("amount-upgrade6"), HTMLcps: document.getElementById("cps-upgrade6"), HTMLcost: document.getElementById("cost-upgrade6"), HTMLid: document.getElementById("upgrade6"), buttonx10: document.getElementById("upgrade6x10"), buttonx100: document.getElementById("upgrade6x100"), buttonxMAX: document.getElementById("upgrade6xMAX"), amount: 0, multiplier: 1.07, cps: 1, cost: 400000},
+	{name: "Upgrade 8", HTMLamount: document.getElementById("amount-upgrade7"), HTMLcps: document.getElementById("cps-upgrade7"), HTMLcost: document.getElementById("cost-upgrade7"), HTMLid: document.getElementById("upgrade7"), buttonx10: document.getElementById("upgrade7x10"), buttonx100: document.getElementById("upgrade7x100"), buttonxMAX: document.getElementById("upgrade7xMAX"), amount: 0, multiplier: 1.07, cps: 1, cost: 2500000},
+	{name: "Upgrade 9", HTMLamount: document.getElementById("amount-upgrade8"), HTMLcps: document.getElementById("cps-upgrade8"), HTMLcost: document.getElementById("cost-upgrade8"), HTMLid: document.getElementById("upgrade8"), buttonx10: document.getElementById("upgrade8x10"), buttonx100: document.getElementById("upgrade8x100"), buttonxMAX: document.getElementById("upgrade8xMAX"), amount: 0, multiplier: 1.07, cps: 1, cost: 15000000},
+	{name: "Upgrade 10",HTMLamount: document.getElementById("amount-upgrade9"), HTMLcps: document.getElementById("cps-upgrade9"), HTMLcost: document.getElementById("cost-upgrade9"), HTMLid: document.getElementById("upgrade9"), buttonx10: document.getElementById("upgrade9x10"), buttonx100: document.getElementById("upgrade9x100"), buttonxMAX: document.getElementById("upgrade9xMAX"), amount: 0, multiplier: 1.07, cps: 1, cost: 100000000}
 ,];
 
 var skills = [
@@ -39,31 +42,58 @@ var skills = [
 	{name: "Skill 5", HTMLcost: document.getElementById("cost-skill4"), HTMLid: document.getElementById("skill4"), duration: 0, cost: 100, coolDown: 0}
 ];
 
-var saveButton = document.getElementById("saveButton");
-var loadButton = document.getElementById("loadButton");
+var features = [
+	{name: "Inventory", 	HTMLbutton: document.getElementById("button-feature0"), HTMLid: document.getElementById("feature0")},
+	{name: "Crafting", 		HTMLbutton: document.getElementById("button-feature1"), HTMLid: document.getElementById("feature1")},
+	{name: "Achievements",	HTMLbutton: document.getElementById("button-feature2"), HTMLid: document.getElementById("feature2")},
+	{name: "Pets", 			HTMLbutton: document.getElementById("button-feature3"), HTMLid: document.getElementById("feature3")},
+	{name: "Quests",		HTMLbutton: document.getElementById("button-feature4"), HTMLid: document.getElementById("feature4")},
+	{name: "Roulette",		HTMLbutton: document.getElementById("button-feature5"), HTMLid: document.getElementById("feature5")},
+	{name: "Events",		HTMLbutton: document.getElementById("button-feature6"), HTMLid: document.getElementById("feature6")},
+	{name: "Shop",			HTMLbutton: document.getElementById("button-feature7"), HTMLid: document.getElementById("feature7")}
+];
 
-var canvas = document.getElementById("canvas");
-var comboSequence = document.getElementById("comboSequence");
-var comboPointsMessage = document.getElementById("comboPointsMessage");
-var clicksCountText = document.getElementById("clicksCount-text");
-var mainButton = document.getElementById("mainButton");
-var loadingBarLeft = document.getElementById("loadingBarLeft");
-var loadingBarRight = document.getElementById("loadingBarRight");
-var alerts = document.getElementById("alerts");
+var menu = [
+	{name: "Profile",	HTMLbutton: document.getElementById("button-menu0"), HTMLid: document.getElementById("menu0"), HTMLclose: document.getElementById("close-profile-menu"), HTMLwindow: document.getElementById("profile-overlay"), HTMLcontent: document.getElementById("profile-overlay-content")},
+	{name: "Log",		HTMLbutton: document.getElementById("button-menu1"), HTMLid: document.getElementById("menu1"), HTMLclose: document.getElementById("close-log-menu"), HTMLwindow: document.getElementById("log-overlay"), HTMLcontent: document.getElementById("log-overlay-content")},
+	{name: "Options",	HTMLbutton: document.getElementById("button-menu2"), HTMLid: document.getElementById("menu2"), HTMLclose: document.getElementById("close-options-menu"), HTMLwindow: document.getElementById("options-overlay"), HTMLcontent: document.getElementById("options-overlay-content")},
+	{name: "Save",		HTMLbutton: document.getElementById("button-menu3"), HTMLid: document.getElementById("menu3"), HTMLclose: document.getElementById("close-save-menu"), HTMLwindow: document.getElementById("save-overlay"), HTMLcontent: document.getElementById("save-overlay-content")},
+	{name: "Load",		HTMLbutton: document.getElementById("button-menu4"), HTMLid: document.getElementById("menu4"), HTMLclose: document.getElementById("close-load-menu"), HTMLwindow: document.getElementById("load-overlay"), HTMLcontent: document.getElementById("load-overlay-content")}
+];
+
+var options = [
+	{name: "Number Notations Toggle", HTMLinput: document.getElementById("number-notation-toggle")}
+];
+
+var comboSequence =			document.getElementById("comboSequence");
+var comboPointsMessage =	document.getElementById("comboPointsMessage");
+var clicksCountText =		document.getElementById("clicksCount-text");
+var mainButton =			document.getElementById("mainButton");
+var loadingBarLeft =		document.getElementById("loadingBarLeft");
+var loadingBarRight =		document.getElementById("loadingBarRight");
+var alerts =				document.getElementById("alerts");
+var marquee =				document.getElementById("marquee");
+var log =					document.getElementById("log");
 
 
 
 //Basically the main method that starts the game
 window.onload = function(){
-	drawCanvas();
 	window.addEventListener('keydown', handleFirstTab);
+	
+	//System initiation
+	initiateCloseAllWindows();
+	initiateMenuButtons();
 	initiateUpgrades();
 	initiateRightClick();
 	initiateCombo();
+
+	//Visuals initiation
 	drawElements();
 	animateButton();
 	checkPrices();
-	comboInterval = setInterval(frame, comboIntervalSpeed);
+
+	alertPlayer("Welcome!");
 };
 
 // What happens when you click the Click button
@@ -89,6 +119,7 @@ function loop() {
 // Updates the clicks count and the CPS of all upgrades
 function buyUpgrade(n) {
 	clicks -= upgrades[n].cost;
+	console.log(upgrades[n].cost);
 
 	upgrades[n].amount++;
 	upgrades[n].HTMLamount.innerHTML = upgrades[n].amount;
@@ -100,6 +131,7 @@ function buyUpgrade(n) {
 // Checks if any of the upgrades and skills are available for purchase
 function checkPrices() {
 	for (let i = upgrades.length - 1; i >= 0; i--) {
+		//Check prices for x1
 		if (clicks >= upgrades[i].cost) {
 			upgrades[i].HTMLid.classList.remove("disabled");
 			upgrades[i].HTMLid.classList.add("enabled");
@@ -110,8 +142,37 @@ function checkPrices() {
 			upgrades[i].HTMLid.disabled = true;
 		}
 
-		if (upgrades[i].amount > 0) {
-			upgrades[i].HTMLamount.style.color = "black";
+		//Check prices for x10
+		if (clicks >= upgrades[i].cost) {
+			upgrades[i].buttonx10.classList.remove("disabled");
+			upgrades[i].buttonx10.classList.add("enabled");
+			upgrades[i].buttonx10.disabled = false;
+		} else {
+			upgrades[i].buttonx10.classList.remove("enabled");
+			upgrades[i].buttonx10.classList.add("disabled");
+			upgrades[i].buttonx10.disabled = true;
+		}
+
+		//Check prices for x100
+		if (clicks >= upgrades[i].cost) {
+			upgrades[i].buttonx100.classList.remove("disabled");
+			upgrades[i].buttonx100.classList.add("enabled");
+			upgrades[i].buttonx100.disabled = false;
+		} else {
+			upgrades[i].buttonx100.classList.remove("enabled");
+			upgrades[i].buttonx100.classList.add("disabled");
+			upgrades[i].buttonx100.disabled = true;
+		}
+
+		//Check prices for xMAX
+		if (clicks >= upgrades[i].cost) {
+			upgrades[i].buttonxMAX.classList.remove("disabled");
+			upgrades[i].buttonxMAX.classList.add("enabled");
+			upgrades[i].buttonxMAX.disabled = false;
+		} else {
+			upgrades[i].buttonxMAX.classList.remove("enabled");
+			upgrades[i].buttonxMAX.classList.add("disabled");
+			upgrades[i].buttonxMAX.disabled = true;
 		}
 	}
 
@@ -130,7 +191,7 @@ function checkPrices() {
 
 // Updates the upgrades' price texts
 function drawElements() {
-	clicksCountText.innerHTML = clicks.toLocaleString("en-EN");
+	clicksCountText.innerHTML = numeral(clicks).format('0a');//clicks.toExponential(2);//.toLocaleString("en-EN");
 	
 	// Loops through all upgrade buttons and updates their CPS value
 	for (var i = upgrades.length - 1; i >= 0; i--) {
@@ -263,9 +324,27 @@ function initiateCombo() {
 
 function initiateUpgrades() {
 	for (let i = upgrades.length - 1; i >= 0; i--) {
-			let upgrade = upgrades[i].HTMLid;
-			upgrade.addEventListener("click", function() {
+			upgrades[i].HTMLid.addEventListener("click", function() {
 				buyUpgrade(i);
+			});
+
+			upgrades[i].buttonx10.addEventListener("click", function() {
+				for (var j = 0; j < 10; j++) {
+					checkPrices();
+					buyUpgrade(i);
+				}
+			});
+
+			upgrades[i].buttonx100.addEventListener("click", function() {
+				for (var j = 0; j < 10; j++) {
+					buyUpgrade(i);
+				}
+			});
+
+			upgrades[i].buttonxMAX.addEventListener("click", function() {
+				for (var j = 0; j < 10; j++) {
+					buyUpgrade(i);
+				}
 			});
 
 			upgrades[i].HTMLcost.innerHTML = upgrades[i].cost.toLocaleString("en-EN");;
@@ -275,7 +354,8 @@ function initiateUpgrades() {
 
 function checkComboCounter() {
 	if (comboCounter >= combo.length) {
-		clicks += 1000;
+		clicks += comboReward;
+		alertPlayer("Combo!");
 		comboPointsAnimation();
 		drawElements();
 		while (comboSequence.firstChild) {
@@ -327,7 +407,7 @@ function comboSpawner() {
 
 }
 
-function frame() {
+function drawComboCooldown() {
         if (loadingBarWidth >= 52) {
         	comboSpawner();
             loadingBarWidth = 1;
@@ -372,6 +452,25 @@ function comboPointsAnimation() {
 }
 
 function alertPlayer(message) {
+	date = new Date();
+	timestamp = "[" + ("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2) + ":" + ("0" + date.getSeconds()).slice(-2) + "]";
+
+	marquee.innerHTML = "";
+	marquee.innerHTML = timestamp + ": " + message;
+
+	const ts = document.createElement("SPAN");
+	const e = document.createElement("SPAN");
+	const logEntry = document.createElement("P");
+
+	ts.innerHTML = timestamp + ": ";
+	ts.style.color = "#aaa";
+	e.innerHTML = message;
+	logEntry.appendChild(ts);
+	logEntry.appendChild(e);
+
+	log.appendChild(document.createElement("HR"));
+	log.appendChild(logEntry);
+	
 	alerts.innerHTML = message;
 	alerts.classList.toggle("show");
 	setTimeout(function() {
@@ -388,48 +487,51 @@ function handleFirstTab(e) {
         document.body.classList.add('user-is-tabbing');
         window.removeEventListener('keydown', handleFirstTab);
     }
+
+    //Disable Arrow Keys for Scrolling
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
 }
 
 
-function drawCanvas() {
+function initiateMenuButtons() {
+	for (let i = menu.length - 1; i >= 0; i--) {
+		menu[i].HTMLbutton.addEventListener("click", function() {
+			menu[i].HTMLwindow.style.display = "block";
+		});
 
-      // Initialise an empty canvas and place it on the page
-      var context = canvas.getContext("2d");
-      canvas.width = 1100;
-      canvas.height = 750;
+		menu[i].HTMLclose.addEventListener("click", function () {
+			menu[i].HTMLwindow.style.display = "none";
+		});
 
-      // Inital starting position
-      var posX = 20,
-          posY = canvas.height / 2;
+		menu[i].HTMLwindow.addEventListener("click", function() {
+    	    menu[i].HTMLwindow.style.display = "none";
+		});
 
-      // Initial velocities
-      var vx = 10,
-          vy = -10,
-          gravity = 1;
+		menu[i].HTMLcontent.addEventListener("click", function(e) {
+			event.stopPropagation();
+		})
+	}
+}
 
-      // Draw shapes on the canvas
-      setInterval(function() {
-        // Erase canvas
-        context.fillStyle = "black";//"#222";
-        context.fillRect(0,0,canvas.width, canvas.height);
-        posX += vx;
-        posY += vy;
+function initiateCloseAllWindows() {
+	window.addEventListener("keydown", function(e) {
+		if (e.keyCode == 27) {
+			for (var i = menu.length - 1; i >= 0; i--) {
+				menu[i].HTMLwindow.style.display = "none";
+			}
+		}
+	});
+}
 
-        if (posY > canvas.height * 0.75) {
-          vy *= -0.6;
-          vx *= 0.75;
-          posY = canvas.height * 0.75;
-        }
+//Options
+function resetOptions() {
+	options[0].HTMLinput.checked = true;
+}
 
-        vy += gravity;
-
-        // Draw a circle particle on the canvas
-        context.beginPath();
-        context.fillStyle = "white";
-        // After setting the fill style, draw an arc on the canvas
-        context.arc(posX, posY, 10, 0, Math.PI*2, true); 
-        context.closePath();
-        context.fill();
-        
-      }, 30);
-    }
+function saveOptions() {
+	if (options[0].HTMLinput.checked) {
+		
+	}
+}
